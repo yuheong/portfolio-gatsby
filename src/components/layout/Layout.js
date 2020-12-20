4fc6e180418284a2ca17e4fd56374cb045178b0b
yuheong <yuhe_ong@hotmail.com>
2020-12-20 15:47:07 +0800

    test netlify

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -9,7 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
-import SEO from "../Seo"
+//import SEO from "../Seo"
 import MyNavbar from "../navbar/MyNavbar"
 import Footer from "./Footer"
 
@@ -26,7 +26,7 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
-        <SEO
+        {/* <SEO
           title="Yu-He Ong Software Developer"
           keywords={[
             `gatsby`,
@@ -43,7 +43,7 @@ const Layout = ({ children }) => (
             `Javascript`,
             `Frontend Developer`,
           ]}
-        />
+        /> */}
         <MyNavbar />
         <div
           style={{
